a99b9e43ff7d093c2092c4c46a8766332caf2ba2
charbel daher <charbeldaher34@gmail.com>
2023-01-05 08:39:03 +0200

    Add project files.

---
+++ AllProducts.cs
@@ -0,0 +1,140 @@
+﻿using Coffee_shop.product;
+using Coffee_shop.shop;
+
+namespace Coffee_shop
+{
+    public partial class AllProducts : Form
+    {
+        public AllProducts() => InitializeComponent();
+
+        void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            Hide();
+            var a = new Admin();
+            a.Show();
+        }
+
+        void LoginForm_Click(object sender, EventArgs e)
+        {
+            Hide();
+            LoginForm a = new();
+            a.Show();
+        }
+
+        void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+
+        void AllProducts_Load_1(object sender, EventArgs e)
+        {
+            Program.daher.BindToDataGridView2(dataGridView1);
+        }
+ 
+        // private void bu(object sender, EventArgs e)
+        // {
+        //    foreach (DataGridViewRow row in dataGridView1.Rows)
+        //    {
+        //        // Get the values of the cells
+        //        string name = (string)row.Cells["Name"].Value;
+        //        string description = (string)row.Cells["Description"].Value;
+        //        double price = Convert.ToDouble(row.Cells["Price"].Value);
+        //        int quantity = Convert.ToInt32(row.Cells["QuantityInStock"].Value);
+        //        List<string> cat = new();
+        //        Dictionary<string, string> att = new();
+        //        int tmp = 0;
+        //        int tmp1 = 0;
+        //        try
+        //        {
+        //            cat = (List<string>)row.Cells["Categories"].Value.ToString().Split(',').ToList();
+        //            tmp = 1;
+        //        }
+        //        catch (Exception)
+        //        {
+        //            if(tmp==0)
+        //            {
+        //                cat = null;
+        //            }
+        //        }
+        //        try
+        //        {
+
+        //            string[] keyValuePairs = row.Cells["Attributes"].Value.ToString().Split(',');
+
+        //            // Create a dictionary to store the key-value pairs
+        //            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+
+        //            // Iterate through the key-value pairs and split each pair by "="
+        //            foreach (string pair in keyValuePairs)
+        //            {
+        //                string[] keyValue = pair.Split('=');
+        //                string key = keyValue[0];
+        //                string value = keyValue[1];
+
+        //                // Add the key and value to the dictionary
+        //                dictionary.Add(key, value);
+        //            }
+        //            att = dictionary;
+        //            tmp1 = 1;
+        //        }
+        //        catch(Exception)
+        //        {
+        //            if(tmp1==0)
+        //            {
+        //                att = null;
+        //            }
+
+        //        }
+
+        //            // Create a new Product object using the values obtained from the cells
+        //            Product product = new Product(name, description, price, quantity, cat, att);
+
+        //            // Add the new Product object to the Shop
+        //            Program.daher.Add(product);
+        //        }
+        //    }
+        void button1_Click(object sender, EventArgs e)
+        {
+            // bu(sender, e);
+        }
+
+        void toolStripMenuItem2_Click(object sender, EventArgs e) => this.Close();
+             public  void sortt()
+        {
+            var comparer = new ProductQuantityComparer();
+
+            Program.daher.Sort(comparer);
+        }
+        public class ProductQuantityComparer : IComparer<Product>
+        {
+            public int Compare(Product x, Product y)
+            {
+                // Compare the quantity in stock of the two products
+                int result = x.QuantityInStock.CompareTo(y.QuantityInStock);
+
+                // If the quantity in stock is equal, compare the names of the products
+                if (result == 0)
+                {
+                    result = x.Name.CompareTo(y.Name);
+                }
+
+                return result;
+            }
+        }
+
+        private void sortbut_Click(object sender, EventArgs e)
+        {
+            sortt();
+            Program.daher.BindToDataGridView2(dataGridView1);
+
+        }
+ 
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Shop a = new(2);
+            a.AddRange(Program.daher.FindAll(x => x.Price < 10));
+            a.BindToDataGridView2(dataGridView1);
+        }
+    }
+
+}
---
+++ User.cs
@@ -0,0 +1,196 @@
+﻿using Coffee_shop.product;
+using FakeItEasy;
+using System.Collections.Generic;
+
+namespace Coffee_shop
+{
+    public partial class User : Form
+    {
+        public User() => InitializeComponent();
+
+        void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedValue = ItemsCb.Text.ToString();
+            ItemPrice.Text = (Program.daher.Find(x => x.Name == selectedValue).Price*Program.daher.Benefitrate).ToString();
+            int qty = Program.daher.Find(x => x.Name == selectedValue).QuantityInStock;
+            QuantityInStock.Text = qty.ToString();
+            NbOrder.Maximum = qty;
+            // Append the selected value to the RichTextBox
+            // ItemsCb.SelectedValueChanged += (sender, e) => RichOrder.AppendText(ItemsCb.SelectedValue.ToString() + "\n");
+            // var lines = RichOrder.Lines.Distinct().ToArray();
+            // RichOrder.Lines = lines;
+        }
+
+        void button1_Click(object sender, EventArgs e) => MessageBox.Show("");
+
+        void Order_Click(object sender, EventArgs e)
+        {
+            string message = "At " + Program.datetime + " ," + Program.User + " has bought";
+
+            foreach (string a in RichOrder.Lines)
+            {
+                if (a != "")
+                {
+                    string[] sf = a.ToString().Split(' ');
+
+                    string name = sf[0];
+                    string q = sf[1];
+                    int qty = Convert.ToInt32(q);
+                    // message += "\n    " + qty.ToString() + " of " + name;
+                    Program.daher.Find(x => x.Name == name).QuantityInStock -= Convert.ToInt32(qty);
+                    message += "\n    " + "The new quantity available of:" + name + " is:" + Program.daher.Find(x => x.Name == name).QuantityInStock.ToString() + " (taken:" + q + ")\n";
+                    // message += "\n    " + "The new qty available is:" + Program.daher.Find(x => x.Name == name).QuantityInStock.ToString();
+                }
+            }
+
+            Program.daher.RemoveOutOfStockProducts(false);
+            Helper.StreamWriter(message, "UserTransactions.txt");
+            ItemsCb.Items.Clear();
+
+            // foreach (Product product in Program.daher)
+            //    ItemsCb.Items.Add(product.Name);
+
+            ResetForm();
+        }
+
+        void ResetForm()
+        {
+            Controls.Clear();
+            InitializeComponent();
+
+            foreach (Product product in Program.daher)
+                ItemsCb.Items.Add(product.Name);
+        }
+
+        void User_Load(object sender, EventArgs e)
+        {
+            ItemsCb.Items.Clear();
+
+            foreach (Product product in Program.daher)
+                ItemsCb.Items.Add(product.Name);
+        }
+
+        void RichOrder_TextChanged(object sender, EventArgs e)
+        {
+            var lines = RichOrder.Lines.Distinct().ToArray();
+            RichOrder.Lines = lines;
+        }
+
+        void AddITem_Click(object sender, EventArgs e)
+        {
+            double bill = 0;
+
+            if (Convert.ToInt32(NbOrder.Value) <= Convert.ToInt32(QuantityInStock.Text.ToString()))
+            {
+                RichOrder.AppendText(ItemsCb.Text.ToString() + ' ' + Convert.ToInt32(NbOrder.Value).ToString() + "\n");
+                OrderRich();
+            }
+            else
+            {
+                MessageBox.Show("There isn't enough in stock!");
+            }
+
+            string s = RichOrder.Text;
+            ResetForm();
+            RichOrder.Text = s;
+
+            foreach (var line in RichOrder.Lines)
+            {
+                if (line != "")
+                {
+                    bill += Convert.ToInt32(line.Split(" ")[1]) * Program.daher.Find(x => x.Name == line.Split(" ")[0]).Price * Program.daher.Benefitrate;
+                }
+            }
+
+            TextBill.Text = bill.ToString();
+        }
+
+        void loginFormToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Hide();
+            LoginForm f = new();
+            f.Show();
+        }
+
+        void QuantityInStock_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+        }
+
+        void OrderRich()
+        {
+            Dictionary<string, string> namesAndValues = new Dictionary<string, string>();
+
+            foreach (string line in RichOrder.Lines)
+            {
+                if (line != "")
+                {
+                    string[] parts = line.Split(' ');
+                    string name = parts[0];
+                    string value = parts[1];
+
+                    if (namesAndValues.ContainsKey(name))
+                    {
+                        namesAndValues[name] = value;
+                    }
+                    else
+                    {
+                        namesAndValues.Add(name, value);
+                    }
+                }
+            }
+
+            RichOrder.Clear();
+
+            foreach (var nameAndValue in namesAndValues)
+            {
+                RichOrder.AppendText(nameAndValue.Key + " " + nameAndValue.Value + Environment.NewLine);
+            }
+        }
+
+        void closeToolStripMenuItem_Click(object sender, EventArgs e) => this.Close();
+
+        private void CoffeeProducts_Click(object sender, EventArgs e)
+        {
+            ItemsCb.Items.Clear();
+            List<Product> a = (List<Product>)Program.daher.FindAll(x => x.Categories.Contains("coffee"));
+            foreach(var x in a)
+            {
+                ItemsCb.Items.Add(x.Name);
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            ItemsCb.Items.Clear();
+            foreach (var x in Program.daher)
+            {
+                ItemsCb.Items.Add(x.Name);
+            }
+        }
+
+        private void ProductInfo_Click(object sender, EventArgs e)
+        {
+            if (ItemsCb.Text != "")
+            {
+                Program.daher.Where(x => x.Name == ItemsCb.Text).FirstOrDefault().Print(true);
+            }
+            else
+            {
+                MessageBox.Show("Choose a Product First!");
+            }
+        }
+
+        private void ItemPrice_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
